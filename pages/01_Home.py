102b8e88c9b326f94b8e00f97748ad2a5542885e
edenede2 <131803027+edenede2@users.noreply.github.com>
2025-04-11 16:05:09 +0300

    fix: Correct user project retrieval and role parsing for accurate display

--- pages/01_Home.py
+++ pages/01_Home.py
@@ -24,12 +24,12 @@ if is_logged_in:
         user_email = st.experimental_user.email
         user_project = st.secrets.get(user_email.split('@')[0], 'None')
         if user_project is not None:
-            user_project = user_project.split(',')[0]
+            user_project = user_project.split(',')[1]
         user_role = st.secrets.get(user_email.split('@')[0], 'Guest')
         st.write(f"Logged in as: {user_email}")
         # st.write(f"Role: {user_role.split(',')[1]}")
         if user_role != 'Guest':
-            user_role = user_role.split(',')[1].strip()
+            user_role = user_role.split(',')[0].strip()
         st.write(f"Role: {user_role}")
         if user_role not in ['Admin', 'Manager']:
             st.warning("You don't have permission to access this page.")
